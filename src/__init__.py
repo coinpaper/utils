9b70138b37cc114c25e90cf28adfffcf3e2aee40
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2019-02-18 21:43:02 +0100

    Export APICoin

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ from .NestedDict import nested_dict_get
 from .ImageCompressor import compress_image
 from .LinearRegression import LinearRegression
 from .DatesList import add_dates_to_weekly_list, list_values_only
-from .APICoin import APIObject
+from .APICoin import APICoin
